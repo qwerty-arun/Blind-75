2865d52fed8112a3c896360c4a2b256324bc785a
qwerty-arun <arunkr.1804@gmail.com>
2025-07-22 23:02:22 +0530

    enocode and decode problem

--- Arrays and Hashing/encode_decode.cpp
+++ Arrays and Hashing/encode_decode.cpp
@@ -15,15 +15,58 @@ Recommended Time and Space: O(m) for each encode and decode and O(m+n)
 #include<string>
 using namespace std;
 
-class Solution1{
+class Solution {
 public:
-    string encode(vector<string>& strs)
-    {
 
+    string encode(vector<string>& strs) {
+        if(strs.empty()) return "";
+        vector<int> sizes;
+        string res = "";
+        for(string& s: strs)
+            sizes.push_back(s.size());
+        
+        for(int sz: sizes)
+            res += to_string(sz) + ',';
+        
+        res += '#';
+        for(string& s: strs)
+            res += s;
+        cout<<res<<endl;
+        return res;
     }
 
-    vector<string> decode(string s)
-    {
-
+    vector<string> decode(string s) {
+        if(s.empty()) return {};
+        vector<int> sizes;
+        vector<string> res;
+        int i = 0;
+        while(s[i] != '#')
+        {
+            string cur = "";
+            while(s[i] != ',')
+            {
+                cur += s[i];
+                i++;
+            }
+            sizes.push_back(stoi(cur));
+            i++;
+        }
+        i++;
+        for(int sz: sizes)
+        {
+            res.push_back(s.substr(i,sz));
+            i += sz;
+        }
+        for(auto value: res)
+            cout<<value<<endl;
+        return res;
     }
 };
+
+int main()
+{
+    Solution soln;
+    vector<string> str = {"neet", "code", "love", "you"};
+    string res = soln.encode(str);
+    vector<string> decoded = soln.decode(res);
+}
